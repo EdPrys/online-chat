07a348070ae26e8f35e0abb11854a21c6de9f664
EdPrys <eduard.prystupa@gmail.com>
2023-05-16 12:08:51 +0300

    change options tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,49 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        slade: {
+        slate: {
           500: "#343C51",
           600: "#3F4A65",
           700: "#404757",
-          800: "#565F76"
+          800: "#565F76",
         },
-        grey: {
+        gray: {
           500: "#908E95",
-          800: "#CCCCCC"
+          800: "#CCCCCC",
         },
         blue: {
           500: "#5585FF",
           600: "#0b5ed7",
         },
         red: {
-          500: "#F5544A"
+          500: "#F5544A",
         },
         green: {
-          500: "#95F54A"
-        }
+          500: "#95F54A",
+        },
       },
       fontFamily: {
-        inter: ['var(--font-inter)', ...fontFamily.sans],
-        icomoon: ['var(--font-icomoon)']
+        inter: ["var(--font-inter)", ...fontFamily.sans],
+        icomoon: ["var(--font-icomoon)"],
       },
       borderRadius: {
-        "3xl": '1.25rem',
-      }
+        "3xl": "1.25rem",
+      },
     },
   },
   plugins: [],
-}
+};
